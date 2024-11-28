3b82f0f9dabe07af56e7583a8f176d002ae58146
Ayush Kumar <socialacc211@gmail.com>
2024-11-28 22:51:13 +0530

    date issue solved

--- client/src/utils/date.ts
+++ client/src/utils/date.ts
@@ -2,7 +2,7 @@
 export function dateHelper(date: string) {
   const newDate = new Date(date)
   const year = newDate.getFullYear()
-  const month = newDate.getMonth()
-  const day = newDate.getDay()
-  return `${day}-${month}-${year}`
+  const month = newDate.getMonth()+1;
+  const dataDate = newDate.getDate()
+  return `${dataDate}-${month}-${year}`
 }
