3429e6909791b497bc3540a9afabfd2039d94088
Ayush Kumar <socialacc211@gmail.com>
2024-11-28 19:43:28 +0530

    update

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,7 @@ import e from "express";
 import {globalErr} from "./utils/globalErrorCatch.js";
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config()
+
 const app = e()
 
 app.use(e.json({
@@ -13,9 +12,6 @@ app.use(e.json({
 app.use(e.urlencoded({extended: true}))
 app.use(cookieParser())
 
-const CLIENT_URI = process.env.CLIENT_URI;
-
-console.log(CLIENT_URI)
 const options = {
   origin: ["https://routemate-client.vercel.app/",],
   methods: ["GET","PUT","POST","DELETE"],
@@ -27,9 +23,9 @@ app.use(cors(options))
 import userRouter from './routes/user.routes.js'
 import rmRouter from './routes/routemate.routes.js'
 
-app.get('/',(req,res)=>{
-  res.status(200).json({"message": "user checked"});
-})
+// app.get('/',(req,res)=>{
+//   res.status(200).json({"message": "user checked"});
+// })
 
 app.use('/user',userRouter)
 app.use('/rm',rmRouter)
--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ import app from './app.js'
 import dbConnect from './db/db.js'
 
 await dbConnect().then( _ => {
-  const PORT = process.env.PORT ?? 6000
+  const PORT = process.env.PORT || 6000
 
   app.listen(PORT,()=>{
     console.log(`server started listening on port: ${PORT}`)
